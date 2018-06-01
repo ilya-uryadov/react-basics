1d68311ddb0bfdbf6e80a125c0c2fa6b389a3120
Урядов Илья Иванович <ilya-79@yandex.ru>
2018-06-01 12:11:57 +0300

    140

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 import Header from './components/Header';
 import Todo from './components/Todo';
 import Form from './components/Form';
@@ -18,52 +18,83 @@ class App extends React.Component {
         this.handleEdit = this.handleEdit.bind(this);
     }
 
-    nextId() {
+    componentDidMount(){
+        //fetch('http://localhost:3000/api/todos') 
+         axios.get('http://localhost:3000/api/todos')
+            .then(response =>response.data)
+            .then(todos => this.setState({todos}))
+            .catch(this.handleError);
+    }
+
+    /*nextId() {
         this._nextId = this._nextId || 4;
 
         return this._nextId++;
-    }
+    }*/
 
     handleAdd(title) {
-        const todo = {
-            id: this.nextId(),
-            title,
-            completed: false
-        };
+        axios.post('/api/todos',{title})
+            .then(response => response.data)
+            .then(todo => {
+                const todos = [...this.state.todos, todo];
 
-        const todos = [...this.state.todos, todo];
+                this.setState({ todos });
+            })
+            .catch(this.handleError);
 
-        this.setState({ todos });
+              
     } 
 
     handleDelete(id) {
-        const todos = this.state.todos.filter(todo => todo.id !== id);
+        axios.delete(`/api/todos/${id}`)
+            .then(() => {
+                const todos = this.state.todos.filter(todo => todo.id !== id);
+                this.setState({ todos });
+                console.log(`/api/todos/${id}`);
+            })
+            .catch(this.handleError);
 
-        this.setState({ todos });
+        
     }
 
     handleToggle(id) {
-        const todos = this.state.todos.map(todo => {
-            if (todo.id === id) {
-                todo.completed = !todo.completed;
-            }
-
-            return todo;
-        });
-
-        this.setState({ todos });
+        axios.patch(`/api/todos/${id}`)
+            .then(response => { 
+                const todos = this.state.todos.map(todo => {
+                    if (todo.id === id) {
+                        todo = response.data; 
+                    }
+                    
+                    return todo;
+                });
+                this.setState({todos});
+            })    
+            .catch(this.handleError);        
+        
     }
 
     handleEdit(id, title) {
-        const todos = this.state.todos.map(todo => {
-            if (todo.id === id) {
-                todo.title = title;
-            }
+        axios.put(`/api/todos/${id}`,{title})
+        .then(response => {
+            const todos = this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo = response.data;
+                    
+                }
+    
+                return todo;
 
-            return todo;
-        });
+            });
+            this.setState({ todos });
+        
+        })
+        .catch(this.handleError);
 
-        this.setState({ todos });
+        
+    }
+    
+    handleError(error){
+        console.log(error);
     }
 
     render() {
